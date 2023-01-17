fe1068429b94565e7fb286a17f84f200ea558152
Tom Teck <tom.teck@gmail.com>
2023-01-17 09:25:13 +0100

    added docstrings

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 
 
 #---PW GENERATOR FUNCTIONS--#
-def generate_pw():
+def generate_pw()->None:
+    """Uses the string module to get the letters and digits that make up the alphabet uses to generate the random characters. These characters are 
+    appended to the pwd string which is then assigned to the session_state variable [pw]"""
     letters = string.ascii_letters
     digits = string.digits  
     alphabet = letters + digits
@@ -34,7 +36,9 @@ def generate_pw():
 
     st.session_state["pw"] = pwd
 
-def get_random_word():
+#---Currently API Ninja Random Word API is used to accomplish generating the random words---#
+def get_random_word()->str:
+    """Uses the API Ninja API to request a word string. This string is then parsed to extract only the word and return it."""
     api_url = 'https://api.api-ninjas.com/v1/randomword'
     response = requests.get(api_url, headers={'X-Api-Key': st.secrets.API_NINJA})
     if response.status_code == requests.codes.ok:
@@ -45,12 +49,13 @@ def get_random_word():
     else:
         return "Error:", response.status_code, response.text
 
-def generate_ps():
+def generate_ps()->None:
+    """Uses the get_random_word function to request five words. These are concatenated into a string with dashes and then 
+    assigned these to the session_state variable [pw] """
     passphrase = ""
     for x in range(5):
         passphrase += f"{get_random_word()}-"
     passphrase_final = passphrase[:-1]  
-    #return passphrase_final
     st.session_state["pw"] = passphrase_final
 
 #---MAIN PAGE---#
@@ -62,9 +67,11 @@ st.title(f"{page_icon} {page_title}")
 col1,col2 = st.columns([4,4], gap = "large")
 
 with col1:
+    st.caption("Secure password length is set at 14 chars.")
     st.button("Generate secure password", key = "pw_button", on_click = generate_pw)
 
 with col2:
+    st.caption("Secure passphrase length is set at 5 words.")
     st.button("Generate secure password sentence", key = "ps_button", on_click = generate_ps)
 "#"
 "#"
